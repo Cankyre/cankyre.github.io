b6cda516fc803d24ab86866b6bfb3a11d905629a
Cankyre <xilefznokan@gmail.com>
2022-02-15 12:01:01 +0100

    made it work i believe

--- main.js
+++ main.js
@@ -2,19 +2,13 @@ const input = document.getElementById("goto-input");
 
 input.addEventListener("keyup", async (ev) => {
   if (ev.key == "Enter" && input.value) {
-    const text = await (
+    const textjson = await (
       await fetch(
-        "https://thingproxy.freeboard.io/fetch/https://rentry.co/cankyre_goto/raw"
+        "https://parpaing-bot.thatcookie.repl.co/goto"
       )
-    ).text();
-    let textjson = {};
-    for (let i in text.split(";")) {
-      textjson[text.split(";")[i].split(":")[0]] = text
-        .split(";")
-        [i].split(":")[1];
-    }
+    ).json();
     if (textjson[input.value]) {
-      window.location.href = "https://" + textjson[input.value];
+      window.location.href = textjson[input.value];
     }
   }
 });
